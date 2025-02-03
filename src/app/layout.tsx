da10f2bd1b80e71d4141a5c4044d77a7a2301e86
Imtiaj Sajin <imtiajsajin@gmail.com>
2025-02-04 01:40:45 +0600

    chatbot added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,29 @@
+"use client";
+
+import { useEffect } from "react";
 import Sidebar from "@/components/Sidebar";
 import Header from "@/components/Header";
 import "@/styles/globals.css";
-import { ReactNode } from "react";
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.innerHTML = `
+      window.embeddedChatbotConfig = {
+        chatbotId: "kZLkaoOx0TUXXUx3hHU2s",
+        domain: "www.chatbase.co"
+      }
+    `;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.src = "https://www.chatbase.co/embed.min.js";
+    script2.setAttribute("chatbotId", "kZLkaoOx0TUXXUx3hHU2s");
+    script2.setAttribute("domain", "www.chatbase.co");
+    script2.defer = true;
+    document.head.appendChild(script2);
+  }, []);
+
   return (
     <html lang="en">
       <body className="flex bg-gray-50">
